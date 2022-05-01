feeb97c2547bf92b3c11174b85080a6801ced631
Benito Ishimwe <benishimwe31@gmail.com>
2022-05-01 16:20:16 +0200

    REST API for post

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv/config');
+
+//Middlewares
+app.use(cors());
+app.use(bodyParser.json());
+
+//Import Routes
+const postsRoute = require('./routes/posts');
+
+app.use('/posts', postsRoute);
+
+// ROUTES
+app.get('/', (req, res) => {
+  res.send('We are on home');
+});
+
+//connect to db
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () =>
+  console.log('connected to DB!')
+);
+
+// How do we start listening to the server
+app.listen(3000);
